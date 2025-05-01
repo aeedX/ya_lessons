2e7c50fef8245bff4562e0a0218d6f3cb708a159
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-01 18:38:16 +0700

    FLASK.WTF_trainings

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
+from flask import Flask, render_template
+import os
 
-import requests
-from PIL import Image
-from map_scaling import get_params
+app = Flask(__name__)
+img = None
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
-toponym_to_find = " ".join(sys.argv[1:])
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof='eng' if ('инженер' in prof or 'строитель in prof') else 'sc')
 
-map_params = get_params(toponym_to_find)
 
-map_api_server = "https://static-maps.yandex.ru/v1"
-response = requests.get(map_api_server, params=map_params)
-im = BytesIO(response.content)
-opened_image = Image.open(im)
-opened_image.show()
+if __name__ == '__main__':
+    app.run()

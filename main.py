dda2597a27123e201aa70c995bed1135a3dc9572
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-07 16:23:08 +0700

    SQLALCHEMY_register-form

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from flask import Flask, render_template, request, redirect
-from loginform import LoginForm
+from flask import Flask, render_template, redirect
+from registerform import RegForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-form = {}
-
-@app.route('/auto_answer', methods=['GET', 'POST'])
-def auto_answer():
-    if request.method == 'GET':
-        return render_template('auto_answer.html')
-    elif request.method == 'POST':
-        form['surname'] = request.form['surname']
-        form['name'] = request.form['name']
-        form['email'] = request.form['email']
-        form['edu'] = request.form['edu']
-        form['prof'] = request.form.getlist('prof')
-        form['sex'] = request.form['sex']
-        form['about'] = request.form['about']
-        form['accept'] = bool(request.form['accept'])
-        return redirect('/answer')
 
 
-@app.route('/answer')
-def answer():
-    return render_template('answer.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def login():
+    form = RegForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('reg.html', form=form)
 
 
 if __name__ == '__main__':

85eebc82ff93ec3578d3a9647953a3cd4f522b8b
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-07 15:24:43 +0700

    FLASK.WTF_double-protection

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from json import load
-
-from flask import Flask, render_template
-from random import choice
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/member')
-def member():
-    with open('templates/members.json', 'r', encoding="UTF8") as f:
-        return render_template('member.html', member=choice(load(f)))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':

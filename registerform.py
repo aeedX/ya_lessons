ce71a3d6e7ec1d6bc378e38e954fb13d0ff27cd8
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-08 11:37:19 +0700

    Update registerform.py

--- registerform.py
+++ registerform.py
@@ -4,13 +4,13 @@ from wtforms.validators import DataRequired
 
 
 class RegForm(FlaskForm):
-    login = StringField('Login/email', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_confirm = PasswordField('Repeat password', validators=[DataRequired()])
-    surname = PasswordField('Surname', validators=[DataRequired()])
-    name = PasswordField('Name', validators=[DataRequired()])
-    age = PasswordField('Age', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
     position = StringField('Position', validators=[DataRequired()])
-    speciality = PasswordField('Speciality', validators=[DataRequired()])
-    address = PasswordField('Address', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
     submit = SubmitField('Submit')

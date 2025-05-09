62d4ffe8ece63caee2b63c554f6d364ca1994239
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-09 20:56:31 +0700

    Update test.py

--- test.py
+++ test.py
@@ -30,3 +30,36 @@ print(post('http://localhost:5000/api/v2/users',
 print(post('http://localhost:5000/api/v2/users', json={}).json())
 # empty request
 
+
+print(get('http://localhost:5000/api/v2/jobs/1').json())
+
+print(get('http://localhost:5000/api/v2/jobs/-1').json())
+# non-existent id
+
+print(get('http://localhost:5000/api/v2/jobs/qwe').json())
+# not int
+
+print(delete('http://localhost:5000/api/v2/jobs/1').json())
+
+print(delete('http://localhost:5000/api/v2/jobs/-1').json())
+# non-existent id
+
+print(delete('http://localhost:5000/api/v2/jobs/qwe').json())
+# not int
+
+
+print(get('http://localhost:5000/api/v2/jobs').json())
+
+
+print(post('http://localhost:5000/api/v2/jobs',
+           json={
+               'team_leader': 1,
+               'job': 'new job',
+               'work_size': 15,
+               'collaborators': '1, 2, 3',
+               'is_finished': False
+           }).json())
+
+print(post('http://localhost:5000/api/v2/jobs', json={}).json())
+# empty request
+

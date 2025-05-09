d1aca8c2e248dca420a2cbc6d0a83fb05e652544
aedX <165893593+aeedX@users.noreply.github.com>
2025-05-09 20:50:13 +0700

    Create jobs_resource.py

---
+++ jobs_resource.py
@@ -0,0 +1,55 @@
+from flask import jsonify
+from flask_restful import reqparse, abort, Resource
+from data.__all_models import Jobs
+from data import db_session
+
+parser = reqparse.RequestParser()
+parser.add_argument('team_leader', type=int, required=True)
+parser.add_argument('job', required=True)
+parser.add_argument('work_size', type=int, required=True)
+parser.add_argument('collaborators', required=True)
+parser.add_argument('is_finished', type=bool, required=True)
+
+
+def abort_if_job_not_found(job_id):
+    session = db_session.create_session()
+    job = session.query(Jobs).get(job_id)
+    if not job:
+        abort(404, message=f"Job {job_id} not found")
+
+
+class JobsResource(Resource):
+    def get(self, job_id):
+        abort_if_job_not_found(job_id)
+        session = db_session.create_session()
+        job = session.query(Jobs).get(job_id)
+        return jsonify({'job': job.to_dict()})
+
+    def delete(self, job_id):
+        abort_if_job_not_found(user_id)
+        session = db_session.create_session()
+        job = session.query(Jobs).get(job_id)
+        session.delete(job)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class JobsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        jobs = session.query(Jobs).all()
+        return jsonify({'jobs': [item.to_dict() for item in jobs]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        job = Jobs(
+            team_leader=args['team_leader'],
+            job=args['job'],
+            work_size=args['work_size'],
+            collaborators=args['collaborators'],
+            is_finished=args['is_finished']
+        )
+        session.add(job)
+        session.commit()
+        return jsonify({'id': job.id})
